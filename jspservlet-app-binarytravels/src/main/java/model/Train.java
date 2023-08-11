d10e122135ab71ef93b694807c73c35a844bfc8b
Iamneo <techadmin@iamneo.ai>
2023-08-11 16:28:24 +0000

    Backup Commit

--- jspservlet-app-binarytravels/src/main/java/model/Train.java
+++ jspservlet-app-binarytravels/src/main/java/model/Train.java
@@ -3,5 +3,42 @@ package model;
 import java.time.LocalDate;
 
 public class Train{
-	//Enter code here...	
+
+    private int noOfPersons;
+    private String breth;
+    private int rates;
+    private LocalDate date;
+    public int getNoOfPersons() {
+        return noOfPersons;
+    }
+    public void setNoOfPersons(int noOfPersons) {
+        this.noOfPersons = noOfPersons;
+    }
+    public String getBreth() {
+        return breth;
+    }
+    public void setBreth(String breth) {
+        this.breth = breth;
+    }
+    public int getRates() {
+        return rates;
+    }
+    public void setRates(int rates) {
+        this.rates = rates;
+    }
+    public LocalDate getDate() {
+        return date;
+    }
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+    public Train(int noOfPersons, String breth, int rates, LocalDate date) {
+        this.noOfPersons = noOfPersons;
+        this.breth = breth;
+        this.rates = rates;
+        this.date = date;
+    }
+
+    
+    //Enter code here...    
 }
